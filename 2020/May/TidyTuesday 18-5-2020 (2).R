1f6ffedf02530036859c1ae8c945fd584bc77df3
JuanmaMN <juanma_mn86@hotmail.com>
2020-06-02 18:12:19 +0100

    update

--- 2020/May/TidyTuesday 18-5-2020 (2).R
+++ 2020/May/TidyTuesday 18-5-2020 (2).R
@@ -1,6 +1,3 @@
-
-# Upload the package ------------------------------------------------------
-
 library(ggtext)
 library(tidyverse)
 library(scales)
@@ -15,12 +12,22 @@ library(scales)
 
 vb_matches <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-19/vb_matches.csv', guess_max = 76000)
 
+View(vb_matches)
 
-# Male --------------------------------------------------------------------
 
-## Prepare the data
+library(ggtext)
+library(tidyverse)
+library(scales)
+library(patchwork)
+library(ggflags)
+library(countrycode)
+library(ggfittext)
+library(scales)
 
-Mdonut<-vb_matches%>%
+
+# Male
+
+MdonutFIVB3<-vb_matches%>%
   mutate(
     Analysis=case_when(
       w_p1_country == w_p2_country ~ "Same",
@@ -28,44 +35,19 @@ Mdonut<-vb_matches%>%
       TRUE ~ as.character(w_p1_country))) %>% 
   filter(bracket == "Winner's Bracket" 
          & round == "Round 1" 
-         & Analysis %in% c("Same", "Different")
          & gender == "M"
-         & circuit == "AVP") %>%
-  group_by(Analysis) %>% 
+         & circuit == "FIVB") %>%
+  group_by(w_p1_country) %>% 
   summarize(total=n()) %>%
   mutate(ftotal=total/sum(total), 
-         pcnt=round(ftotal*100, 1), 
-         yaxismax=cumsum(ftotal), 
-         yaxismin = c(0, head(yaxismax, n=-1)),
-         label_position = (yaxismax+yaxismin)/2) 
-
-
-View(Mdonut)
-
-## Graph
-
-gender_donutMdonut<-ggplot(Mdonut, aes(ymax=yaxismax, ymin=yaxismin, xmax=4, xmin=3, fill=Analysis)) +
-  geom_rect(show.legend=F, alpha=0.5) + 
-  coord_polar(theta="y") +
-  xlim(c(1, 5))+ 
-  theme_void() + 
-  scale_fill_manual(values=c("#CDAA25", "#43a07e")) +
-  geom_text(x=3.5, aes(y=label_position, label=paste(pcnt,"%",sep="")), size=4, col=c("black", "black"))  + 
-  geom_richtext(aes(label="<span style='color:#000000'>AVP circuit<br></span>
-                      <span style='color:#CDAA25'>Different nationality<br></span>
-                      <span style='color:#43a07e'>Same nationality<br></span>",
-                    x=1, y=0),
-                fill=NA, label.color=NA,
-                family="Arial",
-                size=3)  +
-  geom_text(aes(label="Male competition", x=5, y=0), color="#000000", family="Arial Bold", size=4)
+         pcnt=round(ftotal*100, 1)) %>%
+  top_n(8,ftotal)
 
 
 
+MdonutFIVB3$code=c("at","br","de", "nl","no","ru","ch","us")
 
-# Female ------------------------------------------------------------------
-
-Wdonut<-vb_matches%>%
+WdonutFIVB3<-vb_matches%>%
   mutate(
     Analysis=case_when(
       w_p1_country == w_p2_country ~ "Same",
@@ -73,64 +55,95 @@ Wdonut<-vb_matches%>%
       TRUE ~ as.character(w_p1_country))) %>% 
   filter(bracket == "Winner's Bracket" 
          & round == "Round 1" 
-         & Analysis %in% c("Same", "Different")
          & gender == "W"
-         & circuit == "AVP") %>%
-  group_by(Analysis) %>% 
+         & circuit == "FIVB") %>%
+  group_by(w_p1_country) %>% 
   summarize(total=n()) %>%
   mutate(ftotal=total/sum(total), 
-         pcnt=round(ftotal*100, 1), 
-         yaxismax=cumsum(ftotal), 
-         yaxismin = c(0, head(yaxismax, n=-1)),
-         label_position = (yaxismax+yaxismin)/2)
+         pcnt=round(ftotal*100, 1)) %>%
+  top_n(8,ftotal)
+
+
+WdonutFIVB3$code=c("au",
+                   "br",
+                   "ch",
+                   "cz",
+                   "de",
+                   "nl",
+                   "ch",
+                   "us")
+
+
+#WdonutFIVB3  differs from WdonutFIVB2 a bit. The same for M database
 
 
 
-gender_donutWdonut<-ggplot(Wdonut, aes(ymax=yaxismax, ymin=yaxismin, xmax=4, xmin=3, fill=Analysis)) +
-  geom_rect(show.legend=F, alpha=0.5) + 
-  coord_polar(theta="y") + 
-  xlim(c(1, 5))+ 
-  theme_void() + 
-  scale_fill_manual(values=c("#CDAA25", "#43a07e")) +
-  geom_text(x=3.5, aes(y=label_position, label=paste(pcnt,"%",sep="")), size=4, col=c("black", "black"))  + 
-  geom_richtext(aes(label="<span style='color:#000000'>AVP circuit<br></span>
-        <span style='color:#CDAA25'> Different nationality<br></span>
-                      <span style='color:#43a07e'> Same nationality<br></span>
-                      ",
-                    x=1, y=0),
-                fill=NA, label.color=NA,
-                family="Arial",
-                size=3) +
-  geom_text(aes(label="Female competition", x=5, y=0), color="#000000", family="Arial Bold", size=4) 
+ggplotMdonutFIVB2B<-MdonutFIVB3 %>% ggplot(aes(x=fct_reorder(w_p1_country,ftotal), y=ftotal,group = w_p1_country,
+                                               fill= factor(w_p1_country))) +
+  #geom_bar(stat="identity", fill="#69b3a2", width=0.6) +
+  geom_flag(y = -.005, aes(country = code), size = 8, hjust = -2) +
+  #geom_text(nudge_y = 0.2, color = "darkred", size = 5) +
+  scale_fill_manual(values = c( "Brazil" = "#4e79a7",
+                                "United States"      = "#76B7B2",
+                                "Switzerland"     = "#76B7B2",
+                                "Russia" = "#76B7B2",
+                                "Norway" = "#76B7B2",
+                                "Netherlands"      = "#76B7B2",
+                                "Germany" = "#76B7B2",
+                                "Austria" = "#76B7B2"))+
+  coord_flip()+
+  #coord_flip(clip = "off", expand = FALSE)  +
+  geom_col(width = 0.6,show.legend=F) +
+  geom_bar_text(place = "right", contrast = TRUE, size=10,
+                aes(label=paste0(w_p1_country, "  ",pcnt)))  +
+  guides(fill = NULL) +
+  ylim(-0.01, 0.2)+ 
+  theme_void() 
+
+
+ggplotWdonutFIVB2B<-WdonutFIVB3 %>% ggplot(aes(x=fct_reorder(w_p1_country,ftotal), y=ftotal,group = w_p1_country,
+                                               fill= factor(w_p1_country))) +
+  #geom_bar(stat="identity", fill="#69b3a2", width=0.6) +
+  geom_flag(y = -.005, aes(country = code), size = 8, hjust = -2) +
+  #geom_text(nudge_y = 0.2, color = "darkred", size = 5) +
+  scale_fill_manual(values = c( "Brazil" = "#4e79a7",
+                                "United States"= "#76B7B2",
+                                "Switzerland" = "#76B7B2",
+                                "Czech Republic"= "#76B7B2",
+                                "Australia"= "#76B7B2",
+                                "Netherlands"= "#76B7B2",
+                                "Germany" = "#76B7B2",
+                                "China" = "#76B7B2"))+
+  coord_flip()+
+  #coord_flip(clip = "off", expand = FALSE)  +
+  geom_col(width = 0.6,show.legend=F) +
+  geom_bar_text(place = "right", contrast = TRUE, size=10,
+                aes(label=paste0(w_p1_country, "  ",pcnt)))  + 
+  guides(fill = NULL) +
+  ylim(-0.01, 0.2)+ 
+  theme_void() 
 
 
 
+patchwork4 <- ggplotWdonutFIVB2B | ggplotMdonutFIVB2B
 
 
-
-
-patchwork <- gender_donutWdonut | gender_donutMdonut
-
-
-pWM2 <- patchwork + plot_annotation(title = "Beach Volleyball - AVP circuit",
-                                    subtitle = "Analysis of the winners within the AVP circuit\n Percentage of winners where the team had the same and different nationalities",
-                                    caption = "\n Source: TidyTuesday
+PWFIVB <- patchwork4 + plot_annotation(title = "Beach Volleyball - FIVB circuit",
+                                       subtitle = "Country analysis - Percentage of winners - Top 8 countries",
+                                       caption = "\n Source: TidyTuesday
       Visualization: JuanmaMN (Twitter @Juanma_MN)",
-                                    theme = theme(plot.background = element_rect(fill = "#ffffff"),
-                                                  plot.title = element_text(margin = margin(b = 8), 
-                                                                            color = "#000000",face = "bold",size = 12,
-                                                                            hjust = 0.5,
-                                                                            family = "Arial"),
-                                                  plot.subtitle = element_text(margin = margin(t=10,b = 25), 
-                                                                               color = "#000000", size = 10, family = "Arial",
-                                                                               hjust = 0.5),
-                                                  plot.caption =  element_text(margin = margin(t = 20), 
-                                                                               color = "#000000", size = 8, family = "Arial",
-                                                                               hjust = 0.95)))
+                                       theme = theme(plot.background = element_rect(fill = "#f4efe1"),
+                                                     plot.title = element_text(margin = margin(b = 8), 
+                                                                               color = "#000000",face = "bold",size = 12,
+                                                                               hjust = 0.5,
+                                                                               family = "Arial"),
+                                                     plot.subtitle = element_text(margin = margin(t=10,b = 25), 
+                                                                                  color = "#000000", size = 10, family = "Arial",
+                                                                                  hjust = 0.5),
+                                                     plot.caption =  element_text(margin = margin(t = 20), 
+                                                                                  color = "#000000", size = 8, family = "Arial",
+                                                                                  hjust = 0.95)))
 
 
 
-pWM2 
-
-
-
+PWFIVB
