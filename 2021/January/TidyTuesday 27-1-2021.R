2d0d2511157570dcc1f21f50a24aeb8d7a780df2
JuanmaMN <juanma_mn86@hotmail.com>
2021-01-27 19:35:03 +0000

    update

---
+++ 2021/January/TidyTuesday 27-1-2021.R
@@ -0,0 +1,161 @@
+
+# Upload data -------------------------------------------------------------
+
+plastics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-26/plastics.csv')
+
+
+
+# Upload the packages -----------------------------------------------------
+
+pacman::p_load(readxl, lubridate, tidyverse, ggplot2, hrbrthemes, ggfittext, patchwork, hrbrthemes, scales,ggtext, ggpubr,
+               grid, gridtext,hrbrthemes,scales,ggtext, ggpubr, biscale, cowplot,sysfonts,ggflags, showtext)
+
+
+
+# Prepare the data for graph ----------------------------------------------
+
+
+plastics_wrangle_2019<- plastics%>% filter(parent_company == "Grand Total" & year == "2019") %>% 
+  select(country, year, grand_total, volunteers)
+
+plastics_wrangle_2019_bi<-plastics_wrangle_2019 %>% 
+  bi_class(x = grand_total, y = volunteers, style = "quantile", dim = 3)
+
+
+# Fonts -------------------------------------------------------------------
+
+extrafont::loadfonts(device = "win", quiet = TRUE)
+
+font_add_google("Lora")
+
+font_labels <- "Lora"
+
+showtext_auto()
+
+
+# Rename countries (not all  countries are in the dataset)
+
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "United Kingdom" = "UK")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "United States of America" = "USA")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Russian Federation" = "Russia")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Antigua and Barbuda" = "Antigua")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Bahamas, The" = "Bahamas")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Cabo Verde" = "Cape Verde")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Congo, Dem. Rep." = "Democratic Republic of the Congo")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Egypt, Arab Rep." = "Egypt")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Gambia, The" = "Gambia")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Iran, Islamic Rep." = "Iran")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Kyrgyz Republic" = "Kyrgyzstan")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Lao PDR" = "Laos")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Micronesia, Fed. Sts." = "Micronesia")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Sint Maarten (Dutch part)" = "Sint Maarten")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Slovak Republic" = "Slovakia")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Syrian Arab Republic" = "Syria")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Trinidad and Tobago" = "Trinidad")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Venezuela, RB" = "Venezuela")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Virgin Islands (U.S.)" = "Virgin Islands")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Yemen, Rep." = "Yemen")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Korea, Dem. People's Rep." = "North Korea")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Korea, Rep." = "South Korea")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "North Macedonia" = "Macedonia")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Brunei Darussalam" = "Brunei")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Cote D_ivoire" = "Ivory Coast")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Micronesia, Fed. Sts." = "Micronesia")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Yemen, Rep." = "Yemen")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Democratic Republic of Congo" = "Democratic Republic of the Congo")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Micronesia (country)" = "Micronesia")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Saint Vincent and the Grenadines" = "Saint Vincent")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Timor" = "Timor-Leste")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Congo" = "Republic of Congo")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Saint Kitts and Nevis" = "Saint Kitts")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "Taiwan_ Republic of China (ROC)" = "Taiwan")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "NIGERIA" = "Nigeria")
+plastics_wrangle_2019_bi$country <- recode(plastics_wrangle_2019_bi$country, "ECUADOR" = "Ecuador")
+
+
+
+
+
+# Join the data with world data set ---------------------------------------
+
+world <- map_data("world") %>%   filter(region != "Antarctica") 
+
+world_TT_join_bi_plastics <- plastics_wrangle_2019_bi%>%
+  left_join(world, by = c('country'='region'))
+
+
+
+world_TT_join_bi_graphp_plastics <- ggplot() + 
+  geom_map(data = world, map = world,
+           aes(long, lat, group = group,  map_id = region),
+           fill = "#282828", color = "#282828") +
+  geom_map(data =world_TT_join_bi_plastics, map = world,
+           aes(fill = bi_class, map_id = country),
+           color = "#282828", size = 0.15, alpha = .8) +
+  bi_scale_fill(pal = "GrPink", dim = 3, guide = F) +
+  scale_x_continuous(breaks = NULL) + 
+  scale_y_continuous(breaks = NULL) +
+  theme(
+    plot.title = element_text(margin = margin(b = 8), 
+                              color = "#ffffff",face = "bold",size = 9,
+                              hjust = 0.5,
+                              family = font_labels),
+    plot.subtitle = element_text(margin = margin(t=10,b = 25), 
+                                 color = "#ffffff", size = 6, family = font_labels,
+                                 hjust = 0.5),
+    plot.caption =  element_text(margin = margin(t = 20), 
+                                 color = "#ffffff", size = 5, family = font_labels,
+                                 hjust = 0.95),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    legend.title = element_blank(),
+    #legend.position = "none",
+    axis.text.x    = element_blank(),
+    axis.text.y    = element_blank(),
+    panel.background = element_blank(), 
+    panel.grid.major = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor = element_blank(), 
+    plot.background = element_rect(fill = "#f7f7f7"),
+    panel.border = element_blank(),
+    plot.margin = unit(c(1, 1, 1, 1), "cm"),
+    axis.ticks = element_blank()
+  ) 
+
+
+# Legend ------------------------------------------------------------------
+
+legend_TT2_plastics <- 
+  bi_legend(pal = "GrPink",
+            dim = 3,
+            xlab = "Total count (all types of plastic)",
+            ylab = "Number of volunteers",
+            size = 2.5) +
+  theme(rect = element_rect(fill = "grey10"),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        plot.background = element_rect(fill = "#f7f7f7"),
+        axis.title.x = element_text(size = 10,
+                                    family = font_labels,
+                                    color = "grey70"),
+        axis.title.y = element_text(size = 10,
+                                    family = font_labels,
+                                    color = "grey70"),
+        legend.text = element_text(size = 5,family = font_labels),
+        legend.text.align = 0)
+
+
+# Cowplot -----------------------------------------------------------------
+
+map_legend_TT_10_11_plastics <- ggdraw() +
+  draw_plot(world_TT_join_bi_graphp_plastics, 0, 0, 1, 1) +
+  draw_plot(legend_TT2_plastics, 0, 0.1, 0.2, 0.2) +
+  draw_label("Source:Tidy Tuesday\nVisualization: JuanmaMN (Twitter @Juanma_MN)", 
+             color = "grey70", size = 7.5, angle = 0, x = 0.9, y = 0.05, fontfamily = font_labels) +
+  draw_label("Plastic Pollution in 2019", 
+             color = "#e13d3d", size = 10, angle = 0, x =0.5, y = 0.97,fontfamily = font_labels) + 
+  draw_label("Countries with no data in 2019,\n no color has been assigned", 
+             color = "grey70", size = 7.5, angle = 0, x = 0.1, y = 0.05,fontfamily = font_labels) 
+
+
+map_legend_TT_10_11_plastics
